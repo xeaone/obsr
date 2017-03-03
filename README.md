7f0499a98641f9844c6352a3d1f0d1926514d77a
Alexander Elias <alex.steven.elias@gmail.com>
2017-03-03 09:50:18 -0700

    base

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Obsr
 Obsr - A small powerful observable objects arrays
+
+
+## Overview
+Obsr notifies any change to primitive types including collections such as objects and arrays.
+This library uses getters and setters to notify the callback of changes.
+There are two methods that are included on observed objects and arrays (even nested) that notify the callback of any deletions or additions.
+
+
+## Support
+All major browsers that support ES5 including IE9.
+
+
+# Install
+`npm install obsr --save`
---
+++ test/index.js
@@ -0,0 +1,54 @@
+var Obsr = require('../index');
+
+(function() {
+	'use strict';
+	var o = { num: 1, str: 'two', arr: ['zero', 'one', 'two', 'three', 'four'], obj: { hello: 'world'} };
+
+	var observed = Obsr(o, function (key, value) {
+		console.log('|');
+		console.log('cb k: ' + key);
+		console.log('cb v: ' + value);
+		console.log('|');
+	});
+
+
+	// Array Removes
+	// console.log('original length: ' + observed.arr.length);
+	// observed.arr.del(0);
+	// console.log('new length: ' + observed.arr.length);
+
+
+	// Array Inserts
+	// console.log('original length: ' + observed.arr.length);
+	// observed.arr.ins(-1, 'new start');
+	// observed.arr.ins(observed.arr.length, 'new end');
+	// console.log(observed.arr);
+	// console.log('new length: ' + observed.arr.length);
+
+
+	// Object Removes
+	// console.log('original length: ' + observed.arr.length);
+	// observed.arr.del(0);
+	// console.log('new length: ' + observed.arr.length);
+
+
+	// Object Inserts
+	observed.obj.ins('moo', { doo: 'poo' });
+	console.log('original value: ' + observed.obj.moo.doo);
+	observed.obj.moo.doo = 'oop';
+	console.log('new value: ' + observed.obj.moo.doo);
+
+	// observed.obj.ins('foo', 'bar');
+	// console.log('new value: ' + observed.obj.foo);
+
+}());
+
+/*
+
+	['zero', 'one', 'two', 'three', 'four']
+	   0       1      2      3        4
+
+	['zero', 'one', 'three', 'four']
+	   0       1       2       3
+
+*/
